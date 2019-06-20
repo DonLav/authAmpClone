2f775ee615ab0158511ff6a7de6db6453a817200
Callum Laverick <callum@wonderlane.co.uk>
2019-06-20 16:03:03 +0100

    Added bootstrap and React Bootstrap. Added a bootstrap navigation bar to App.js

--- src/App.js
+++ src/App.js
@@ -5,15 +5,38 @@ import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth, Analytics } from 'aws-amplify';
 import awsconfig from './aws-exports'; //analytics
 import aws_exports from './aws-exports';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button'
+
 Amplify.configure(aws_exports);
 Amplify.configure(awsconfig);//analytics
 
 
 class App extends Component {
+  handleSelect(eventKey) {
+    alert(`selected ${eventKey}`);
+  }
+
   render() {
     return (
       <div className="App">
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">SCP</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Search</Button>
+          </Form>
+        </Navbar>
         <header className="App-header">
+          <br />
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
